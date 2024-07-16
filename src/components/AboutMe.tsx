8dea418fe3122b75eb3f820417a41c67cd8d4987
Vedbhanushali <19ce005@charusat.edu.in>
2024-07-17 01:16:04 +0530

    About Me completed, Projectdetails WIP

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,42 +1,61 @@
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+interface TeamMemberProps {
+  name: string;
+  role: string;
+  photoUrl: string;
+  description: string;
+  linkedin: string;
+  github: string;
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({
+  name,
+  role,
+  photoUrl,
+  description,
+  linkedin,
+  github,
+}) => {
+  return (
+    <div className="w-full flex flex-row items-center justify-between bg-secondary shadow-lg rounded-lg overflow-hidden">
+      <img className="w-1/2 h-auto" src={photoUrl} alt={name} />
+      <div className="flex flex-col justify-center p-4">
+        <div className="font-bold text-xl text-foreground">{name}</div>
+        <p className="text-base mb-2 text-foreground">{role}</p>
+        <p className="text-sm text-foreground">{description}</p>
+        <div className="flex gap-4 mt-4">
+          <a href={linkedin} target="_blank" rel="noopener noreferrer">
+            <img src="/icons/linkedin.svg" alt="LinkedIn" />
+          </a>
+          <a href={github} target="_blank" rel="noopener noreferrer">
+            <img src="/icons/github.svg" alt="Github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const myDetails = {
+  name: "Ved Bhanushali",
+  role: "Software Developer",
+  photoUrl: "/images/profile.jpg",
+  description:
+    "Specializes in full stack development with expertise in building and maintaining web applications.",
+  linkedin: "https://www.linkedin.com/in/ved-k-bhanushali/",
+  github: "https://github.com/Vedbhanushali",
+};
 
 export default function AboutMe() {
   return (
-    <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
-      <CardHeader className="pb-3">
-        <CardTitle>Ved Bhanushali</CardTitle>
-        <CardDescription className="max-w-xl text-balance leading-relaxed">
-          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-            Software Developer - Working hard to make the world a better place.
-          </h4>
-          I believe in the power of open source and the collaborative spirit it
-          fosters.That's why this project{" "}
-          <blockquote className="mt-6 border-l-2 pl-6 italic">
-            Typing made fun
-          </blockquote>{" "}
-          is open source. I invite you to contribute, share your insights, and
-          help make this project even better. Let's build something amazing
-          together!
-        </CardDescription>
-      </CardHeader>
-      <CardFooter className="p-4">
-        <Button
-          onClick={() => {
-            chrome.tabs.create({
-              url: "https://github.com/Vedbhanushali/typing-made-fun",
-            });
-          }}
-        >
-          Github
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="container mx-auto px-4">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl text-foreground font-bold">About Me</h1>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8">
+        <div className="w-full md:w-3/4 lg:w-3/4">
+          <TeamMember key={myDetails.name} {...myDetails} />
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ src/components/ProjectDetails.tsx
@@ -0,0 +1,40 @@
+// import {
+//   Card,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Button } from "@/components/ui/button";
+
+// return (
+//   <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+//     <CardHeader className="pb-3">
+//       <CardTitle>Ved Bhanushali</CardTitle>
+//       <CardDescription className="max-w-xl text-balance leading-relaxed">
+//         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+//           Software Developer - Working hard to make the world a better place.
+//         </h4>
+//         I believe in the power of open source and the collaborative spirit it
+//         fosters.That's why this project{" "}
+//         <blockquote className="mt-6 border-l-2 pl-6 italic">
+//           Typing made fun
+//         </blockquote>{" "}
+//         is open source. I invite you to contribute, share your insights, and
+//         help make this project even better. Let's build something amazing
+//         together!
+//       </CardDescription>
+//     </CardHeader>
+//     <CardFooter className="p-4">
+//       <Button
+//         onClick={() => {
+//           chrome.tabs.create({
+//             url: "https://github.com/Vedbhanushali/typing-made-fun",
+//           });
+//         }}
+//       >
+//         Github
+//       </Button>
+//     </CardFooter>
+//   </Card>
+// );
