0aacd4ce1a8f7557b26552f7e4ea4aa42e0fc278
Vedbhanushali <19ce005@charusat.edu.in>
2024-07-23 01:13:36 +0530

    Blocklist Design/UI is ready.

--- src/components/BlockList.tsx
+++ src/components/BlockList.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function BlockList() {
-  return <h1>Block List data</h1>;
+  return (
+    <div className="grid w-full gap-2">
+      <Textarea
+        placeholder="Enter Domain example -&#10;google.com&#10;youtube.com"
+        className="text-foreground"
+      />
+      <Button>Save</Button>
+    </div>
+  );
 }
