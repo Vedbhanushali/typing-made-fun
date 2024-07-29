f3737d36e17641d0ff92644dafe77a80e08ac8f8
Vedbhanushali <19ce005@charusat.edu.in>
2024-07-29 18:51:53 +0530

    complete flow of end, start and typing with card chaning

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -16,51 +16,57 @@ const card = [
 ];
 
 export default function TypingTest() {
-  const [currentCard, setCurrentCard] = useState(0); // number of cards
-  const [cursorPosition, setCursorPosition] = useState(card[0].length - 2); // position of cursor in current card
-  const [testStatus, setTestStatus] = useState(0); // 0 not started, 1 started, 2 finished
+  const [currentCard, setCurrentCard] = useState(2); // number of cards
+  const [cursorPosition, setCursorPosition] = useState(card[2].length - 2); // position of cursor in current card
+  const [testStatus, setTestStatus] = useState(0); //0: not started, 1: started, 2: ended
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       if (testStatus == 0 || testStatus == 2) return;
-
       const key = event.key;
-      if (currentCard == card.length) {
-        setTestStatus(2);
-        return;
-      }
+      if (currentCard >= card.length) return;
+      if (cursorPosition >= card[currentCard].length) return;
 
-      if (cursorPosition >= card[currentCard].length) {
-        setCurrentCard(currentCard + 1);
-        setCursorPosition(0);
-      }
       if (key == card[currentCard][cursorPosition]) {
         setCursorPosition(cursorPosition + 1);
       }
     };
+    if (currentCard == card.length) {
+      setTestStatus(2);
+    }
+    if (cursorPosition >= card[currentCard].length) {
+      setCurrentCard(currentCard + 1);
+      setCursorPosition(0);
+    }
+    if (currentCard == card.length) {
+      setTestStatus(2);
+    }
     window.addEventListener("keydown", handleKeyPress);
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
   }, [testStatus, currentCard, cursorPosition]);
-
+  console.log(testStatus, currentCard, cursorPosition);
   const renderTextWithCursor = () => {
     return (
       <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+        test
         <CardHeader>
           <CardDescription className="max-w-lg text-balance leading-relaxed text-justify">
             <span className="text-foreground">
-              {card[currentCard]
-                .split("")
-                .filter((x, index) => index < cursorPosition)
-                .map((char) => char)}
+              {currentCard < card.length &&
+                card[currentCard]
+                  .split("")
+                  .filter((x, index) => index < cursorPosition)
+                  .map((char) => char)}
             </span>
             <span className="blinking-cursor"></span>
             <span className="text-untyped">
-              {card[currentCard]
-                .split("")
-                .filter((char, index) => index >= cursorPosition)
-                .map((char) => char)}
+              {currentCard < card.length &&
+                card[currentCard]
+                  .split("")
+                  .filter((char, index) => index >= cursorPosition)
+                  .map((char) => char)}
             </span>
           </CardDescription>
         </CardHeader>
@@ -83,7 +89,9 @@ export default function TypingTest() {
               <div
                 className={cn(
                   "w-12 h-12 flex items-center justify-center rounded shadow",
-                  key === card[currentCard][cursorPosition].toUpperCase()
+                  currentCard < card.length &&
+                    cursorPosition < card[currentCard].length &&
+                    key === card[currentCard][cursorPosition].toUpperCase()
                     ? "bg-primary"
                     : "bg-gray-300"
                 )}
@@ -99,40 +107,61 @@ export default function TypingTest() {
 
   return (
     <div className="flex flex-col items-center justify-between h-full max-h-screen">
-      {renderTextWithCursor()}
       {testStatus == 0 && (
-        <Card x-chunk="dashboard-02-chunk-0" className="rounded-md">
-          <CardHeader className="p-2 pt-0 md:p-4">
-            <CardTitle>Typing test</CardTitle>
-          </CardHeader>
-          <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-            <Button
-              size="default"
-              className="w-full"
-              onClick={() => {
-                setTestStatus(1);
-              }}
-            >
-              Start
-            </Button>
-          </CardContent>
-        </Card>
+        <>
+          {renderTextWithCursor()}
+          <Card x-chunk="dashboard-02-chunk-0" className="rounded-md">
+            <CardHeader className="p-2 pt-0 md:p-4">
+              <CardTitle>Typing test</CardTitle>
+            </CardHeader>
+            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+              <Button
+                size="default"
+                className="w-full"
+                onClick={() => {
+                  setTestStatus(1);
+                }}
+              >
+                Start
+              </Button>
+            </CardContent>
+          </Card>
+        </>
       )}
-      {testStatus == 1 && renderKeyboardUI()}
-      {testStatus == 2 && (
-        <Card x-chunk="dashboard-02-chunk-0" className="rounded-md">
-          <CardHeader className="p-2 pt-0 md:p-4">
-            <CardTitle>Typing test</CardTitle>
-          </CardHeader>
-          <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+      {testStatus == 1 && (
+        <>
+          {renderTextWithCursor()}
+          {renderKeyboardUI()}
+          <div className="w-full flex justify-end border-t border-foreground pt-4">
             <Button
               size="default"
-              className="w-full"
+              className="w-auto"
               onClick={() => {
-                console.log("test ended");
+                setTestStatus(2);
               }}
             >
               End
+            </Button>
+          </div>
+        </>
+      )}
+      {testStatus == 2 && (
+        <Card x-chunk="dashboard-02-chunk-0" className="rounded-md">
+          <CardHeader className="p-2 pt-0 md:p-4">
+            <CardTitle>Typing Test Over</CardTitle>
+          </CardHeader>
+          <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+            Want to retry
+            <Button
+              size="default"
+              className="w-full"
+              onClick={() => {
+                setTestStatus(0);
+                setCurrentCard(0);
+                setCursorPosition(0);
+              }}
+            >
+              Retry
             </Button>
           </CardContent>
         </Card>
